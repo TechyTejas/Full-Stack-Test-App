d4d602a754fbc3f20bb779e140cffe623beb0769
tejas <sadadetej.27@gmail.com>
2026-07-12 10:23:19 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const port = 5000;
+const app = express();
+const db = require('./static/js/utils/db-connection');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes 
+
+
+//use routes
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
